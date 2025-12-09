73f1728b31a0a66ada6c08ef5ecea4bf0d1aaced
Tung Duong <issoftvn.duong@gmail.com>
2025-12-09 09:55:25 +0700

    Update class Profile

--- src/main/java/com/gtd/profile/entity/Profile.java
+++ src/main/java/com/gtd/profile/entity/Profile.java
@@ -8,23 +8,23 @@ public class Profile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username", nullable = false)
-    private String username;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false)
-    private String password;
-
     public Profile() {
     }
 
-    public Profile(Long id, String username, String email, String password) {
+    public Profile(Long id, String firstName, String lastName, String email) {
         this.id = id;
-        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
-        this.password = password;
     }
 
     public Long getId() {
@@ -35,12 +35,20 @@ public class Profile {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -49,13 +57,5 @@ public class Profile {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
