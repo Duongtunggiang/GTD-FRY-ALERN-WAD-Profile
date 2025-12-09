b5c06185f8bb28dbbbf5dacb2951b08cd51a1206
Tung Duong <issoftvn.duong@gmail.com>
2025-12-09 09:23:03 +0700

    Form project Java spring

---
+++ src/main/java/com/gtd/profile/entity/Profile.java
@@ -0,0 +1,61 @@
+package com.gtd.profile.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "Profile")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Profile() {
+    }
+
+    public Profile(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/gtd/profile/entity/User.java
@@ -0,0 +1,17 @@
+package com.gtd.profile.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role = "USER";
+}
